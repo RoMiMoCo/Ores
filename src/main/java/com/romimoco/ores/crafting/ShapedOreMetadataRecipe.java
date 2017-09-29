c5fe99fb715940a99f204f2a0a13702028deff0a
the_rooster <jroster76+romimoco@gmail.com>
2017-09-29 08:09:59 -0700

    Wasted enough time on the recipe, just doing it the hacky way

--- src/main/java/com/romimoco/ores/crafting/ShapedOreMetadataRecipe.java
+++ src/main/java/com/romimoco/ores/crafting/ShapedOreMetadataRecipe.java
@@ -1,13 +1,12 @@
 package com.romimoco.ores.crafting;
 
-import com.romimoco.ores.Items.BaseIngot;
 import com.romimoco.ores.Items.BaseIngotWithVariants;
+import com.romimoco.ores.util.OreLogger;
 import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.NonNullList;
+import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.World;
-import net.minecraftforge.common.ForgeHooks;
+import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.oredict.ShapedOreRecipe;
 
 public class ShapedOreMetadataRecipe extends ShapedOreRecipe {
@@ -15,17 +14,6 @@ public class ShapedOreMetadataRecipe extends ShapedOreRecipe {
 
     public ShapedOreMetadataRecipe(ResourceLocation group, ItemStack result, Object... recipe) {
         super(group, result, recipe);
-    }
-
-    public boolean matches(InventoryCrafting inv, World world){
-        for (int x = 0; x <= MAX_CRAFT_GRID_WIDTH - width; x++) {
-            for (int y = 0; y <= MAX_CRAFT_GRID_HEIGHT - height; ++y) {
-
-
-            }
-
-        }
-        return false;
     }
 
     @Override
@@ -40,19 +28,53 @@ public class ShapedOreMetadataRecipe extends ShapedOreRecipe {
             if(itemstack.isEmpty()){
                 continue;
             }
-            count++;
             if(itemstack.getItem() instanceof BaseIngotWithVariants){
+               count++;
                meta += 5 - itemstack.getMetadata();
             }else{
-                meta += 5; //if not one of my ingots, assume full ingot
+                //TODO: Filter for item to compare against item in itemstack
+                if(!OreDictionary.getOres("stickWood").contains(itemstack)) { //make sure it's not a stick
+                    count++;
+                    meta += 5; //if not one of my ingots, assume full ingot
+                }
             }
        }
        int maxDmg = res.getMaxDamage();
-
-       meta = meta / 5 * count; //percentage of the total durability that should be kept
-       res.setItemDamage((int)(maxDmg * meta));
-
+       meta = meta / (5 * count); //percentage of the total durability that should be kept
+       res.setItemDamage(maxDmg - (int)(maxDmg * meta));
        return res;
    }
 
+   @Override
+   protected boolean checkMatch(InventoryCrafting inv, int startX, int startY, boolean mirror)
+    {
+        for (int x = 0; x < MAX_CRAFT_GRID_WIDTH; x++)
+        {
+            for (int y = 0; y < MAX_CRAFT_GRID_HEIGHT; y++)
+            {
+                int subX = x - startX;
+                int subY = y - startY;
+                Ingredient target = Ingredient.EMPTY;
+
+                if (subX >= 0 && subY >= 0 && subX < width && subY < height)
+                {
+                    if (mirror)
+                    {
+                        target = input.get(width - subX - 1 + subY * width);
+                    }
+                    else
+                    {
+                        target = input.get(subX + subY * width);
+                    }
+                }
+
+                if (!target.apply(inv.getStackInRowAndColumn(x, y)))
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
 }
