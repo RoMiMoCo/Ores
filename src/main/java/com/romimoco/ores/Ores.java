5e72876050347a8288e925a1daabc1b58ab957f7
the_rooster <jroster76+romimoco@gmail.com>
2017-10-27 22:36:45 -0700

    This deserves a roll to 0.2.2

--- src/main/java/com/romimoco/ores/Ores.java
+++ src/main/java/com/romimoco/ores/Ores.java
@@ -19,7 +19,7 @@ public class Ores
 {
     public static final String MODID = "romimocoores";
     public static final String NAME = "ores";
-    public static final String VERSION = "0.2.0";
+    public static final String VERSION = "0.2.2";
 
 
     @Mod.Instance
