98a9b88a6e965380f81110206d44d5f2bc465b75
Jonathan Roster <jroster76@gmail.com>
2018-03-05 22:43:46 -0800

    update version

--- src/main/java/com/romimoco/ores/Ores.java
+++ src/main/java/com/romimoco/ores/Ores.java
@@ -26,7 +26,7 @@ public class Ores
 {
     public static final String MODID = "romimocoores";
     public static final String NAME = "ores";
-    public static final String VERSION = "0.3.0";
+    public static final String VERSION = "0.3.1";
 
 
     @Mod.Instance
