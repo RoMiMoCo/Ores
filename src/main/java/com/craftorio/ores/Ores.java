16ebebb0c5068019ca284d1fe915c943cbb30c0d
Sergey Cherepanov <sergey.cherepanov@ewave.com>
2020-01-06 07:17:37 +0400

    bump version

--- src/main/java/com/craftorio/ores/Ores.java
+++ src/main/java/com/craftorio/ores/Ores.java
@@ -22,7 +22,7 @@ import net.minecraftforge.registries.IForgeRegistry;
 public class Ores {
     public static final String MODID = "ores";
     public static final String NAME = "ores";
-    public static final String VERSION = "0.5.0";
+    public static final String VERSION = "0.5.1";
 
 
     @Mod.Instance
